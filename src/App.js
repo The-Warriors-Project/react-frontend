59462a0e4c64f0e75d704c8145f9e296fe305d4c
Jacky Wong <hw2813@columbia.edu>
2022-10-11 18:29:51 -0400

    added router barebone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import { BookList } from "./pages/BookList";
+import { Home } from "./pages/Home";
+import { User } from "./pages/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+          <li>
+            <Link to="/User">User</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/books" element={<BookList />}></Route>
+        <Route path="/User" element={<User />}></Route>
+      </Routes>
+    </Fragment>
   );
 }
 
