14ebe37ce3ff2dd45d04fe48743b76d7217aeb31
Jacky Wong <hw2813@columbia.edu>
2022-11-14 17:29:44 -0500

    added search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import BookIcon from "@mui/icons-material/Book";
 import { Stack } from "@mui/system";
 import { useUser } from "../context/UserContext";
+import SearchBar from "./SearchBar";
 
 const styles = {
   icon: {
@@ -24,6 +25,16 @@ const styles = {
 
 function NavBar() {
   const { user, setUser } = useUser();
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchBar = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const submitSearchBar = (e) => {
+    e.preventDefault();
+    console.log(searchValue);
+  };
 
   const logout = () => {
     setUser(null);
@@ -36,28 +47,35 @@ function NavBar() {
         <Typography variant="h6" sx={styles.typography}>
           Reader Hub
         </Typography>
-        {user ? (
-          <Stack direction="row" spacing={2}>
-            <Button color="inherit" component={RouterLink} to="/books">
-              Books
-            </Button>
-            <Button color="inherit" component={RouterLink} to="/user">
-              Profile
-            </Button>
-            <Button color="inherit" onClick={logout}>
-              Logout
-            </Button>
-          </Stack>
-        ) : (
-          <Stack direction="row" spacing={2}>
-            <Button color="inherit" component={RouterLink} to="/signup">
-              SignUp
-            </Button>
-            <Button color="inherit" component={RouterLink} to="/login">
-              Login
-            </Button>
-          </Stack>
-        )}
+        <Stack direction="row" spacing={2}>
+          <SearchBar
+            value={searchValue}
+            onChange={handleSearchBar}
+            onSubmit={submitSearchBar}
+          ></SearchBar>
+          {user ? (
+            <>
+              <Button color="inherit" component={RouterLink} to="/books">
+                Books
+              </Button>
+              <Button color="inherit" component={RouterLink} to="/user">
+                Profile
+              </Button>
+              <Button color="inherit" onClick={logout}>
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" component={RouterLink} to="/signup">
+                SignUp
+              </Button>
+              <Button color="inherit" component={RouterLink} to="/login">
+                Login
+              </Button>
+            </>
+          )}
+        </Stack>
       </Toolbar>
     </AppBar>
   );
