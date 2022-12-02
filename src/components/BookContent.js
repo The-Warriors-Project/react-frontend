d70bbf6294562abc4d7029dc4a5f9c2757a288e1
Jacky Wong <hw2813@columbia.edu>
2022-12-02 09:46:13 -0500

    refactored

--- src/components/BookContent.js
+++ src/components/BookContent.js
@@ -1,5 +1,8 @@
 import { Button, Grid, Rating, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+
+import { useUser } from "../context/UserContext";
 
 const styles = {
   bookInfoSection: {
@@ -13,68 +16,88 @@ const styles = {
     height: "80%",
     overflow: "auto",
   },
+  bookSubstance: {
+    height: "100%",
+    paddingRight: 4,
+  },
+};
+
+const bookData = {
+  imageUrl: "https://m.media-amazon.com/images/I/91asIC1fRwL.jpg",
+  title: "Book Title Goes Here",
+  rating: 2.5,
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam risus mattis libero eleifend tristique. Suspendisse potenti. Nam ac mauris bibendum, mattis urna eu, hendrerit turpis. Donec sagittis sapien id mi sodales sagittis. Ut ornare placerat elit in scelerisque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce lacinia ultricies suscipit. Nullam vel convallis lectus. Ut metus erat, pellentesque id lueleifend tristique. Suspendisse potenti. Nam ac mauris bibendum, mattis urna eu, hendrerit turpis. Donec sagittis sapien id mi sodales sagittis. Ut ornare placerat elit in scelerisque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce lacinia ultricies suscipit. Nullam vel convallis lectus. Ut metus erat, pellentesque id luctus sed, volutpat sit amet arcu. Vestibulum malesuada felis justo, a mollis odio porttitor eget. Quisque placerat laoreet dignissim. Ut venenatis orci velit, non mattis erat porta eget. Curabitur interdum porta fermentum. Cras a libero convallis nisi pharetra pellentesque vel ac odio. Nullam lectus lectus, rutrum quis libero vel, elementum fermentum ipsum.",
+  authors: ["Jordi Adoumie", "Liron Hayon", "Omer Mustel"],
+};
+
+const BookImage = (props) => {
+  const { imageUrl } = props;
+  return <img src={imageUrl} style={styles.image} alt="book cover"></img>;
+};
+
+const authorsToText = (authors) => {
+  return authors && authors.length ? authors.join(", ") : "";
+};
+
+const BookSubstance = (props) => {
+  const { title, authors, rating, description } = props;
+  const { user } = useUser();
+  const navigate = useNavigate();
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="space-between"
+      sx={styles.bookSubstance}
+    >
+      <Grid item style={{ height: "25%" }}>
+        <Typography noWrap variant="h4">
+          {title}
+        </Typography>
+        <Typography noWrap variant="subtitle1">
+          by {authorsToText(authors)}
+        </Typography>
+        <Rating defaultValue={rating} precision={0.5} readOnly />
+      </Grid>
+      <Grid item style={{ height: "60%", overflow: "auto" }}>
+        <Typography variant="h6">Description</Typography>
+        <Box sx={styles.description}>
+          <Typography variant="body2">{description}</Typography>
+        </Box>
+      </Grid>
+      <Grid item style={{ height: "10%" }}>
+        {user ? (
+          <Button variant="contained">Rate and Review!</Button>
+        ) : (
+          <Button
+            variant="contained"
+            onClick={() => {
+              navigate("/signup");
+            }}
+          >
+            Login and Review!
+          </Button>
+        )}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default function BookContent() {
   return (
-    <Grid container spacing={4} sx={styles.bookInfoSection} r>
+    <Grid container spacing={4} sx={styles.bookInfoSection}>
       <Grid item xs={4} style={{ height: "100%" }}>
-        <img
-          src="https://m.media-amazon.com/images/I/91asIC1fRwL.jpg"
-          style={styles.image}
-          alt="book cover"
-        ></img>
+        <BookImage imageUrl={bookData.imageUrl}></BookImage>
       </Grid>
       <Grid item xs={8} style={{ height: "100%" }}>
-        <Grid container direction="column" style={{ height: "100%" }}>
-          <Grid item>
-            <Typography noWrap variant="h4">
-              Book Title Goes Here
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography noWrap variant="subtitle1">
-              by Author1, Author2
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Rating defaultValue={2.5} precision={0.5} readOnly />
-          </Grid>
-          <Grid item style={{ height: "60%", overflow: "auto" }}>
-            <Typography variant="body1">Description</Typography>
-            <Box sx={styles.description}>
-              <Typography variant="body2">
-                Since this Jolt-award winning classic was last updated in 2008,
-                the Java programming environment has changed dramatically. Java
-                7 and Java 8 introduced new features and functions including,
-                forEach() method in Iterable interface, default and static
-                methods in Interfaces, Functional Interfaces and Lambda
-                Expressions, Java Stream API for Bulk Data Operations on
-                Collections, Java Time API, Collection API improvements,
-                Concurrency API improvements, and Java IO improvements. In this
-                new edition of Effective Java, Bloch explores new design
-                patterns and language idioms that have been introduced since the
-                second edition was released in 2008 shortly after Java SE6,
-                including Lambda, streams, generics and collections, as well as
-                selected Java 9 features. As in previous editions, each chapter
-                consists of several “items” presented in the form of a short,
-                standalone essay that provides specific advice, insight into
-                Java platform subtleties, and updated code examples. The
-                comprehensive descriptions and explanations for each item
-                illuminate what to do, what not to do, and why. Updated
-                techniques and best practices on classic topics, including
-                objects, classes, libraries, methods, and serialization How to
-                avoid the traps and pitfalls of commonly misunderstood
-                subtleties of the language Focus on the language and its most
-                fundamental libraries: java.lang, java.util, and, to a lesser
-                extent, java.util.concurrent and java.io
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Button variant="contained">Rate and Review!</Button>
-          </Grid>
-        </Grid>
+        <BookSubstance
+          title={bookData.title}
+          authors={bookData.authors}
+          rating={bookData.rating}
+          description={bookData.description}
+        ></BookSubstance>
       </Grid>
     </Grid>
   );
