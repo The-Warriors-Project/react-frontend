09de8ed65f52de2d817fa20534e67bdc1c2855aa
Jacky Wong <hw2813@columbia.edu>
2022-12-10 19:57:22 -0500

    connected add review function on frontend

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,14 +1,35 @@
-import { Box, Button, Grid, Rating, TextField } from "@mui/material";
-import { Stack } from "@mui/system";
+import { Button, Grid, Rating, TextField } from "@mui/material";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { addReview } from "../api/BooksAPI";
+import { useSnackbar } from "../context/SnackbarContext";
+
+import { useUser } from "../context/UserContext";
 
 export default function ReviewForm() {
   const [rating, setRating] = useState(3);
   const [reviewText, setReviewText] = useState("");
+  const { openSuccessMessage, openErrorMessage } = useSnackbar();
 
-  const handleSubmit = () => {
-    console.log("rating", rating);
-    console.log("reviewText", reviewText);
+  const { id } = useParams();
+  const { user } = useUser();
+
+  const handleSubmit = async () => {
+    const reviewInfo = {
+      book_id: id,
+      review_text: reviewText,
+      username: user.username,
+      score: rating,
+    };
+
+    const handleSuccess = () => {
+      openSuccessMessage("Successfully reviewed and rated!");
+    };
+    const handleFailure = () => {
+      openErrorMessage("Something went wrong. Please try later.");
+    };
+
+    await addReview(reviewInfo, handleSuccess, handleFailure);
   };
 
   return (
