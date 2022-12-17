9064c7773e7786b220aed3802aac1c8e28d89d25
Jacky Wong <hw2813@columbia.edu>
2022-12-17 10:13:16 -0500

    added like button

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,25 +1,33 @@
-import {Button, Grid, Rating, TextField} from "@mui/material";
-import {useState} from "react";
-import {useParams} from "react-router-dom";
-import {addReview} from "../api/ReviewsAPI";
-import {useSnackbar} from "../context/SnackbarContext";
-import {getUserInfoUsername} from "../api/UsersAPI";
-import {useUser} from "../context/UserContext";
+import { Button, Grid, IconButton, Rating, TextField } from "@mui/material";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { addReview } from "../api/ReviewsAPI";
+import { useSnackbar } from "../context/SnackbarContext";
+import { getUserInfoUsername } from "../api/UsersAPI";
+import { useUser } from "../context/UserContext";
+import { Stack } from "@mui/system";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
 
 export default function ReviewForm(props) {
-  const {fetchData, title} = props;
+  const { fetchData, title } = props;
 
   const [rating, setRating] = useState(3);
   const [reviewText, setReviewText] = useState("");
-  const {openSuccessMessage, openErrorMessage} = useSnackbar();
+  const [liked, setLiked] = useState(false);
+  const { openSuccessMessage, openErrorMessage } = useSnackbar();
 
-  const {id} = useParams();
-  const {user} = useUser();
+  const { id } = useParams();
+  const { user } = useUser();
+
+  const handleLikeClicked = () => {
+    setLiked(!liked);
+    console.log("liked", !liked);
+  };
 
   const handleSubmit = async () => {
     const email = await getUserInfoUsername(user.username).then((userInfo) => {
-      return userInfo.email
-    })
+      return userInfo.email;
+    });
     const reviewInfo = {
       book_id: id,
       title: title,
@@ -47,8 +55,8 @@ export default function ReviewForm(props) {
           id="outlined-multiline-flexible"
           label="Add Your Review"
           multiline
-          rows={1.5}
-          sx={{width: "100%", height: "100%"}}
+          rows={2}
+          sx={{ width: "100%", height: "100%" }}
           value={reviewText}
           onChange={(e) => {
             setReviewText(e.target.value);
@@ -56,15 +64,30 @@ export default function ReviewForm(props) {
         />
       </Grid>
       <Grid container item xs={4}>
-        <Rating
-          name="simple-controlled"
-          value={rating}
-          onChange={(event, newRating) => {
-            setRating(newRating);
-          }}
-        />
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="baseline"
+          spacing={2}
+          sx={{ width: "100%" }}
+        >
+          <Rating
+            name="simple-controlled"
+            value={rating}
+            onChange={(event, newRating) => {
+              setRating(newRating);
+            }}
+          />
+          <IconButton
+            aria-label="favorite"
+            color="error"
+            onClick={handleLikeClicked}
+          >
+            {liked ? <Favorite /> : <FavoriteBorder />}
+          </IconButton>
+        </Stack>
         <Button
-          sx={{width: "100%"}}
+          sx={{ width: "100%" }}
           variant="contained"
           onClick={handleSubmit}
         >
