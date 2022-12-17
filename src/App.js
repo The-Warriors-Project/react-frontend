7fd1f82bfb1c8ef59bb4aefa351f7024e916bc84
Solomon Chang <sjc2233@columbia.edu>
2022-12-17 16:12:19 -0500

    first attempt at verification

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { CssBaseline, Container } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
 
 import { Book } from "./pages/Book";
 import { Home } from "./pages/Home";
@@ -7,6 +7,7 @@ import { NotFound } from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import BackgroundVideo from "./components/BackgroundVideo";
 import SignUp from "./pages/SignUp";
+import Verify from "./pages/Verify";
 import SignIn from "./pages/SignIn";
 import { MyReviews } from "./pages/MyReviews";
 
@@ -17,45 +18,46 @@ import CustomizedSnackbars from "./components/SnackBar";
 import { MyBookShelf } from "./pages/MyBookShelf";
 
 const styles = {
-  container: {
-    my: 4,
-    backgroundColor: "background.paper",
-  },
-  video: {
-    width: "100%",
-    height: "100vh",
-    position: "absolute",
-    top: 0,
-    zIndex: -1,
-    objectFit: "cover",
-  },
+    container: {
+        my: 4,
+        backgroundColor: "background.paper",
+    },
+    video: {
+        width: "100%",
+        height: "100vh",
+        position: "absolute",
+        top: 0,
+        zIndex: -1,
+        objectFit: "cover",
+    },
 };
 
 function App() {
-  return (
-    <>
-      <CssBaseline />
-      <SnackbarProvider>
-        <UserProvider>
-          <BackgroundVideo />
-          <Navbar />
-          <CustomizedSnackbars></CustomizedSnackbars>
-          <Container component="main" sx={styles.container}>
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/books/:id" element={<Book />}></Route>
-              <Route path="/myreviews" element={<MyReviews />}></Route>
-              <Route path="/mybookshelf" element={<MyBookShelf />}></Route>
-              <Route path="/signup" element={<SignUp />}></Route>
-              <Route path="/login" element={<SignIn />}></Route>
-              <Route path="/search" element={<SearchedResults />}></Route>
-              <Route path="/*" element={<NotFound />}></Route>
-            </Routes>
-          </Container>
-        </UserProvider>
-      </SnackbarProvider>
-    </>
-  );
+    return (
+        <>
+            <CssBaseline/>
+            <SnackbarProvider>
+                <UserProvider>
+                    <BackgroundVideo/>
+                    <Navbar/>
+                    <CustomizedSnackbars></CustomizedSnackbars>
+                    <Container component="main" sx={styles.container}>
+                        <Routes>
+                            <Route path="/" element={<Home/>}></Route>
+                            <Route path="/books/:id" element={<Book/>}></Route>
+                            <Route path="/myreviews" element={<MyReviews/>}></Route>
+                            <Route path="/mybookshelf" element={<MyBookShelf/>}></Route>
+                            <Route path="/signup" element={<SignUp/>}></Route>
+                            <Route path="/signup/verify" element={<Verify/>}></Route>
+                            <Route path="/login" element={<SignIn/>}></Route>
+                            <Route path="/search" element={<SearchedResults/>}></Route>
+                            <Route path="/*" element={<NotFound/>}></Route>
+                        </Routes>
+                    </Container>
+                </UserProvider>
+            </SnackbarProvider>
+        </>
+    );
 }
 
 export default App;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -9,208 +9,208 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
-import {
-  isValidEmail,
-  isValidUsername,
-  isValidFirstLastName,
-  isValidPassword,
-} from "../util";
+import { isValidEmail, isValidFirstLastName, isValidPassword, isValidUsername, } from "../util";
 import { useSnackbar } from "../context/SnackbarContext";
 import { signup } from "../api/UsersAPI";
 import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  const { openSuccessMessage, openErrorMessage } = useSnackbar();
-  const navigate = useNavigate();
+    const { openSuccessMessage, openErrorMessage } = useSnackbar();
+    const navigate = useNavigate();
 
-  const [formValues, setFormValues] = useState({
-    username: "",
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-  });
+    const [formValues, setFormValues] = useState({
+        username: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+    });
 
-  const [formErrors, setFormErrors] = useState({
-    username: "",
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-  });
+    const [formErrors, setFormErrors] = useState({
+        username: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+    });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
 
-  const validate = () => {
-    setFormErrors((prevFormErrors) => ({
-      ...prevFormErrors,
-      username: isValidUsername(formValues.username) ? "" : "Invalid username",
-    }));
+    const validate = () => {
+        setFormErrors((prevFormErrors) => ({
+            ...prevFormErrors,
+            username: isValidUsername(formValues.username) ? "" : "Invalid username",
+        }));
 
-    setFormErrors((prevFormErrors) => ({
-      ...prevFormErrors,
-      firstName: isValidFirstLastName(formValues.firstName)
-        ? ""
-        : "Invalid first name",
-    }));
+        setFormErrors((prevFormErrors) => ({
+            ...prevFormErrors,
+            firstName: isValidFirstLastName(formValues.firstName)
+                ? ""
+                : "Invalid first name",
+        }));
 
-    setFormErrors((prevFormErrors) => ({
-      ...prevFormErrors,
-      lastName: isValidFirstLastName(formValues.lastName)
-        ? ""
-        : "Invalid last name",
-    }));
+        setFormErrors((prevFormErrors) => ({
+            ...prevFormErrors,
+            lastName: isValidFirstLastName(formValues.lastName)
+                ? ""
+                : "Invalid last name",
+        }));
 
-    setFormErrors((prevFormErrors) => ({
-      ...prevFormErrors,
-      email: isValidEmail(formValues.email) ? "" : "Invalid email",
-    }));
+        setFormErrors((prevFormErrors) => ({
+            ...prevFormErrors,
+            email: isValidEmail(formValues.email) ? "" : "Invalid email",
+        }));
 
-    setFormErrors((prevFormErrors) => ({
-      ...prevFormErrors,
-      password: isValidPassword(formValues.password) ? "" : "Invalid password",
-    }));
+        setFormErrors((prevFormErrors) => ({
+            ...prevFormErrors,
+            password: isValidPassword(formValues.password) ? "" : "Invalid password",
+        }));
+
+        return (
+            isValidUsername(formValues.username) &&
+            isValidFirstLastName(formValues.firstName) &&
+            isValidFirstLastName(formValues.lastName) &&
+            isValidEmail(formValues.email) &&
+            isValidPassword(formValues.password)
+        );
+    };
+
+    const handleSignupMessage = async () => {
+        await signup(
+            formValues,
+            (successMsg) => {
+                openSuccessMessage(successMsg + " Redirecting shortly...");
+                setTimeout(() => {
+                    navigate("/signup/verify", {
+                        state: {
+                            username: formValues.username,
+                            email: formValues.email,
+                        }
+                    });
+                }, 3000);
+            },
+            openErrorMessage
+        );
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (validate()) {
+            await handleSignupMessage();
+        }
+    };
 
     return (
-      isValidUsername(formValues.username) &&
-      isValidFirstLastName(formValues.firstName) &&
-      isValidFirstLastName(formValues.lastName) &&
-      isValidEmail(formValues.email) &&
-      isValidPassword(formValues.password)
+        <Container maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="username"
+                                label="Username"
+                                name="username"
+                                autoComplete="username"
+                                onChange={handleChange}
+                                value={formValues.username}
+                                error={!!formErrors.username}
+                                helperText={formErrors.username}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                autoComplete="given-name"
+                                name="firstName"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                onChange={handleChange}
+                                value={formValues.firstName}
+                                error={!!formErrors.firstName}
+                                helperText={formErrors.firstName}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Last Name"
+                                name="lastName"
+                                autoComplete="family-name"
+                                onChange={handleChange}
+                                value={formValues.lastName}
+                                error={!!formErrors.lastName}
+                                helperText={formErrors.lastName}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                onChange={handleChange}
+                                value={formValues.email}
+                                error={!!formErrors.email}
+                                helperText={formErrors.email}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                                onChange={handleChange}
+                                value={formValues.password}
+                                error={!!formErrors.password}
+                                helperText={formErrors.password}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="/login" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
     );
-  };
-
-  const handleSignupMessage = async () => {
-    await signup(
-      formValues,
-      (successMsg) => {
-        openSuccessMessage(successMsg + " Redirecting shortly...");
-        setTimeout(() => {
-          navigate("/login");
-        }, 3000);
-      },
-      openErrorMessage
-    );
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (validate()) {
-      await handleSignupMessage();
-    }
-  };
-
-  return (
-    <Container maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-                onChange={handleChange}
-                value={formValues.username}
-                error={!!formErrors.username}
-                helperText={formErrors.username}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="given-name"
-                name="firstName"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                onChange={handleChange}
-                value={formValues.firstName}
-                error={!!formErrors.firstName}
-                helperText={formErrors.firstName}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="family-name"
-                onChange={handleChange}
-                value={formValues.lastName}
-                error={!!formErrors.lastName}
-                helperText={formErrors.lastName}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                onChange={handleChange}
-                value={formValues.email}
-                error={!!formErrors.email}
-                helperText={formErrors.email}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="new-password"
-                onChange={handleChange}
-                value={formValues.password}
-                error={!!formErrors.password}
-                helperText={formErrors.password}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign Up
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link href="/login" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Container>
-  );
 }
