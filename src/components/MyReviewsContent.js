b048ccdefe685ce4def636303cdac8f632a2f9bb
Jacky Wong <hw2813@columbia.edu>
2022-12-21 12:46:37 -0500

    fixed reviews overlay issue

--- src/components/MyReviewsContent.js
+++ src/components/MyReviewsContent.js
@@ -1,6 +1,6 @@
-import {Grid, Paper, Rating, Stack, Typography} from "@mui/material";
-import {Box} from "@mui/system";
-import {Link} from "react-router-dom";
+import { Grid, Paper, Rating, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { Link } from "react-router-dom";
 
 const styles = {
   reviewCard: {
@@ -14,38 +14,38 @@ const styles = {
 };
 
 const ReviewedBook = (props) => {
-  const {bookName, imageUrl, rating, reviewText, bookID} = props;
+  const { bookName, imageUrl, rating, reviewText, bookID } = props;
   const bookUrl = "/books/" + bookID;
 
   return (
     <Paper elevation={1} sx={styles.reviewCard}>
-      <Grid container spacing={4} direction="row" style={{height: "100%"}}>
-        <Grid item xs={4} style={{height: "100%"}}>
+      <Grid container spacing={4} direction="row" style={{ height: "100%" }}>
+        <Grid item xs={4} style={{ height: "100%" }}>
           <Stack
             direction="column"
             justifyContent="space-between"
             spacing={2}
-            style={{height: "100%"}}
+            style={{ overflow: "hidden" }}
           >
-            <div style={{height: "20%"}}>
+            <div style={{ height: "20%", width: "inherit" }}>
               <Typography
                 variant="h6"
                 noWrap
                 component={Link}
                 to={bookUrl}
-                sx={{textDecoration: "none", color: "inherit"}}
+                sx={{ textDecoration: "none", color: "inherit" }}
               >
                 {bookName}
               </Typography>
             </div>
             <img
               src={imageUrl}
-              style={{height: "90%", width: "fit-content"}}
+              style={{ height: "5rem", width: "fit-content" }}
             ></img>
           </Stack>
         </Grid>
-        <Grid item xs={8} style={{height: "100%"}}>
-          <Rating name="half-rating" value={rating} precision={0.5} readOnly/>
+        <Grid item xs={8} style={{ height: "100%" }}>
+          <Rating name="half-rating" value={rating} precision={0.5} readOnly />
           <Box style={styles.reviewContent}>
             <Typography>{reviewText}</Typography>
           </Box>
@@ -56,7 +56,7 @@ const ReviewedBook = (props) => {
 };
 
 export default function MyReviewsContent(props) {
-  const {reviewsData} = props;
+  const { reviewsData } = props;
   if (!reviewsData) {
     return <></>;
   }
@@ -64,7 +64,7 @@ export default function MyReviewsContent(props) {
   return (
     <Grid container direction="column" spacing={2}>
       {reviewsData.map(
-        ({score, review_text, book_id, bookName, imageUrl}, idx) => {
+        ({ score, review_text, book_id, bookName, imageUrl }, idx) => {
           return (
             <Grid item key={idx}>
               <ReviewedBook
