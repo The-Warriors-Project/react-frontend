409a2ed1092df92ef57ea026cda818ea99f8b411
Jacky Wong <hw2813@columbia.edu>
2022-12-21 12:55:39 -0500

    upadted format

--- src/components/MyBookshelfContent.js
+++ src/components/MyBookshelfContent.js
@@ -1,10 +1,19 @@
-import {Button, Card, CardActions, CardContent, CardMedia, Grid, Rating, Typography,} from "@mui/material";
-import {Container, Stack} from "@mui/system";
-import {Link} from "react-router-dom";
-import {useUser} from "../context/UserContext";
-import {getLikedBooksByUsername} from "../api/CompositeAPI";
-import {getReviewsByBookID} from "../api/ReviewsAPI";
-import {useEffect, useState} from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Rating,
+  Typography,
+} from "@mui/material";
+import { Container, Stack } from "@mui/system";
+import { Link } from "react-router-dom";
+import { useUser } from "../context/UserContext";
+import { getLikedBooksByUsername } from "../api/CompositeAPI";
+import { getReviewsByBookID } from "../api/ReviewsAPI";
+import { useEffect, useState } from "react";
 
 const styles = {
   cardGrid: {
@@ -41,10 +50,10 @@ const styles = {
 };
 
 function MediaCard(props) {
-  const {name, rating, imageUrl, _id} = props;
+  const { name, rating, imageUrl, _id } = props;
   return (
     <Card sx={styles.card}>
-      <CardMedia component="img" sx={styles.cardMedia} image={imageUrl}/>
+      <CardMedia component="img" sx={styles.cardMedia} image={imageUrl} />
       <CardContent sx={styles.cardContent}>
         <Stack direction="column">
           <Typography
@@ -56,7 +65,7 @@ function MediaCard(props) {
           >
             {name}
           </Typography>
-          <Rating value={rating} readOnly></Rating>
+          <Rating value={rating} readOnly precision={0.5}></Rating>
         </Stack>
       </CardContent>
       <CardActions sx={styles.cardActions}>
@@ -69,55 +78,61 @@ function MediaCard(props) {
 }
 
 export default function MyBookShelfContent() {
-  const {user} = useUser();
+  const { user } = useUser();
   const [bookshelf, setBookshelf] = useState([]);
 
   const fetchData = async () => {
-    getLikedBooksByUsername(user.username).then(async (likedBooks) => {
-      const promises = likedBooks.map(async (likedBook) => {
-        let avgRating = await getReviewsByBookID(likedBook._id).then((result) => {
-          const {average_score} = result;
-          return average_score > 0 ? average_score : 0;
-        }).catch((e) => {
-          console.log(e);
+    getLikedBooksByUsername(user.username)
+      .then(async (likedBooks) => {
+        const promises = likedBooks.map(async (likedBook) => {
+          let avgRating = await getReviewsByBookID(likedBook._id)
+            .then((result) => {
+              const { average_score } = result;
+              return average_score > 0 ? average_score : 0;
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+          const entry = {
+            title: likedBook.title,
+            picture: likedBook.picture,
+            rating: avgRating,
+            _id: likedBook._id,
+          };
+          return entry;
         });
-        const entry = {
-          title: likedBook.title,
-          picture: likedBook.picture,
-          rating: avgRating,
-          _id: likedBook._id,
-        };
-        return entry;
+        const data = await Promise.all(promises);
+        if (data.length) {
+          setBookshelf(data);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
       });
-      const data = await Promise.all(promises);
-      if (data.length) {
-        setBookshelf(data);
-      }
-    }).catch((e) => {
-      console.log(e);
-    });
   };
 
-  useEffect(() => {
+  useEffect(
+    () => {
       fetchData();
     }, // eslint-disable-next-line react-hooks/exhaustive-deps
-    []);
+    []
+  );
 
   return (
     // <Container sx={styles.cardGrid}>
-      <Grid container spacing={4}>
-        {bookshelf.map(({title, rating, picture, _id}, idx) => (
-          <Grid item xs={12} sm={6} md={3} key={idx}>
-            <MediaCard
-              name={title}
-              rating={rating}
-              imageUrl={picture}
-              key={idx}
-              _id={_id}
-            ></MediaCard>
-          </Grid>
-        ))}
-      </Grid>
+    <Grid container spacing={4}>
+      {bookshelf.map(({ title, rating, picture, _id }, idx) => (
+        <Grid item xs={12} sm={6} md={3} key={idx}>
+          <MediaCard
+            name={title}
+            rating={rating}
+            imageUrl={picture}
+            key={idx}
+            _id={_id}
+          ></MediaCard>
+        </Grid>
+      ))}
+    </Grid>
     // </Container>
   );
 }
